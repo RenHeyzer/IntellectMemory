1b19d523add40c5a12cf60703065c53bad249e6a
Admin <baconnkl@gmail.com>
2022-01-20 23:38:35 +0300

    fragment home

---
+++ app/src/main/java/com/geektech/intellect_memort/ui/fragments/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.geektech.intellect_memort.ui.fragments.home
+
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.geektech.intellect_memort.R
+import com.geektech.intellect_memort.common.base.BaseFragment
+import com.geektech.intellect_memort.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
+    R.layout.fragment_home
+) {
+    override val binding by viewBinding(FragmentHomeBinding::bind)
+    override val viewModel: HomeViewModel by viewModels()
+}
