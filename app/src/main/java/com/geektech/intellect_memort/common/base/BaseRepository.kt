5f09b7b570c3b83adc8ae3da8533b0253afc1ba0
Admin <baconnkl@gmail.com>
2022-01-20 23:37:42 +0300

    added BaseRepository and BaseViewModel

---
+++ app/src/main/java/com/geektech/intellect_memort/common/base/BaseRepository.kt
@@ -0,0 +1,58 @@
+package com.geektech.intellect_memort.common.base
+
+import com.geektech.intellect_memort.common.resource.Resource
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentSnapshot
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
+import java.util.*
+
+abstract class BaseRepository {
+
+    protected fun <T> doRequest(request: suspend () -> T) = flow {
+        emit(Resource.Loading())
+        emit(Resource.Success(data = request()))
+    }.catch { message ->
+        emit(
+            Resource.Error(
+                data = null, message = message.localizedMessage ?: "Error Occurred!"
+            )
+        )
+    }
+
+    suspend inline fun <reified T> fetchList(collection: CollectionReference) =
+        collection.get().await().documents.mapNotNull { doc ->
+            doc.toObject(T::class.java)
+        }
+
+    suspend fun addDocument(
+        collection: CollectionReference,
+        hashMap: HashMap<String, Any>,
+        title: String? = null
+    ): Boolean {
+        return try {
+            if (title != null) {
+                collection
+                    .document(title)
+                    .set(hashMap)
+                    .await()
+            } else {
+                collection
+                    .document()
+                    .set(hashMap)
+                    .await()
+            }
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    suspend fun getDocument(collection: CollectionReference, id: String): DocumentSnapshot =
+        collection
+            .document(id)
+            .get()
+            .await()
+
+}
---
+++ app/src/main/java/com/geektech/intellect_memort/common/base/BaseViewModel.kt
@@ -0,0 +1,36 @@
+package com.geektech.intellect_memort.common.base
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.geektech.intellect_memort.common.resource.Resource
+import com.geektech.intellect_memort.ui.fragments.state.UIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+open class BaseViewModel @Inject constructor() : ViewModel(){
+
+    protected fun <T> MutableLiveData<UIState<T>>.subscribeTo(
+        request: () -> Flow<Resource<T>>,
+    ) {
+        viewModelScope.launch {
+            request().collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        this@subscribeTo.value = UIState.Loading()
+                    }
+                    is Resource.Error -> it.message?.let { error ->
+                        this@subscribeTo.value = UIState.Error(error)
+                    }
+                    is Resource.Success -> it.data?.let { data ->
+                        this@subscribeTo.value = UIState.Success(data)
+                    }
+                }
+            }
+        }
+    }
+}
