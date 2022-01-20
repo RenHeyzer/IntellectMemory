4ae6bcd618a52ea0eaae47f6d7638ed86263c3df
Admin <baconnkl@gmail.com>
2022-01-21 00:41:07 +0300

    added navigate and singleClickListener

--- app/src/main/java/com/geektech/intellect_memort/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/geektech/intellect_memort/ui/fragments/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.geektech.intellect_memort.ui.fragments.home
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.intellect_memort.R
 import com.geektech.intellect_memort.common.base.BaseFragment
+import com.geektech.intellect_memort.common.extension.setOnSingleClickListener
 import com.geektech.intellect_memort.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,4 +15,36 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
 ) {
     override val binding by viewBinding(FragmentHomeBinding::bind)
     override val viewModel: HomeViewModel by viewModels()
+
+
+    override fun setupListeners() {
+        setUpBtnPictureListener()
+        setUpBtnRandomNumberListener()
+        setUpBtnBinaryNumbersListener()
+        setUpBtnPlayingCardsListener()
+    }
+
+    private fun setUpBtnPictureListener() {
+        binding.btnPicture.setOnSingleClickListener {
+            findNavController().navigate(R.id.pictureFragment)
+        }
+    }
+
+    private fun setUpBtnRandomNumberListener() {
+        binding.btnRandomNumbers.setOnSingleClickListener {
+            findNavController().navigate(R.id.randomNumbersFragment)
+        }
+    }
+
+    private fun setUpBtnBinaryNumbersListener() {
+        binding.btnBinaryRandomNumbers.setOnSingleClickListener {
+            findNavController().navigate(R.id.binaryNumbersFragment)
+        }
+    }
+
+    private fun setUpBtnPlayingCardsListener() {
+        binding.btnPlayingCards.setOnSingleClickListener {
+            findNavController().navigate(R.id.playingCardsFragment)
+        }
+    }
 }
