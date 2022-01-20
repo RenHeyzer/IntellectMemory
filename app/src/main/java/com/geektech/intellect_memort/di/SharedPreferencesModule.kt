ea4bc4f9980eea07220de9e8a57f52a2f2bf7695
Admin <baconnkl@gmail.com>
2022-01-20 23:42:17 +0300

    added Module for Preferences

---
+++ app/src/main/java/com/geektech/intellect_memort/di/SharedPreferencesModule.kt
@@ -0,0 +1,26 @@
+package com.geektech.intellect_memort.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.geektech.intellect_memort.data.local.sharedpreferences.PreferencesHelper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SharedPreferencesModule {
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences =
+        context.getSharedPreferences("geektech.preferences", Context.MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun provideName(preferences: SharedPreferences) = PreferencesHelper(preferences)
+
+}
