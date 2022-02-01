3f32c2c51deefcd7e2421d10296f66f5fdabe296
RenHeyzer <vesolyyradin@gmail.com>
2022-02-01 18:52:46 +0600

    for list of students recycler

---
+++ app/src/main/java/com/geektech/intellect_memort/domain/models/StudentsModel.kt
@@ -0,0 +1,13 @@
+package com.geektech.intellect_memort.domain.models
+
+import com.geektech.intellect_memort.common.base.IBaseDiffModel
+
+data class StudentsModel(
+
+    val fullName: String? = null,
+    override val password: String? = null,
+    val teacher: String? = null,
+    val branch: String? = null,
+    val location: String? = null,
+    val points: Int? = null,
+) : IBaseDiffModel
