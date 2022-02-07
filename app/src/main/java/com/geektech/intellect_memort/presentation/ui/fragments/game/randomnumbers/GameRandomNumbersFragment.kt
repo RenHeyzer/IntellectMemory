7c29e4b4f9c855d589ac35a7c8460217f2ceb6dd
Admin <baconnkl@gmail.com>
2022-02-07 15:45:44 +0300

    вынес генерацию во ViewModel

--- app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/game/randomnumbers/GameRandomNumbersFragment.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/game/randomnumbers/GameRandomNumbersFragment.kt
@@ -1,5 +1,6 @@
 package com.geektech.intellect_memort.presentation.ui.fragments.game.randomnumbers
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -7,12 +8,11 @@ import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.intellect_memort.R
 import com.geektech.intellect_memort.common.base.BaseFragment
+import com.geektech.intellect_memort.common.extension.gone
 import com.geektech.intellect_memort.common.extension.setOnSingleClickListener
 import com.geektech.intellect_memort.databinding.FragmentGameRandomNumbersBinding
 import com.geektech.intellect_memort.presentation.adapters.RandomNumbersAdapter
-import com.geektech.intellect_memort.presentation.models.RandomNumbersModel
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 @AndroidEntryPoint
 class GameRandomNumbersFragment :
@@ -22,37 +22,59 @@ class GameRandomNumbersFragment :
     override val binding by viewBinding(FragmentGameRandomNumbersBinding::bind)
     override val viewModel: GameRandomNumbersViewModel by viewModels()
     private val args: GameRandomNumbersFragmentArgs by navArgs()
-    private val list = ArrayList<RandomNumbersModel>()
-    private var row: Int = 1
-    private val adapter = RandomNumbersAdapter()
+    private var row: Int = 7
+    private var last: Int = 14
+    private var adapter: RandomNumbersAdapter? = null
 
     override fun initialize() {
+        adapter = RandomNumbersAdapter()
         binding.rvGameRandomNumber.adapter = adapter
-        binding.txtTimer.base = System.currentTimeMillis() + args.time.toLong()
-        binding.txtTimer.start()
+        viewModel.createRandomNumbers(args.quantitynumber)
     }
 
     override fun setupListeners() {
-        val random = List(args.quantitynumber) { Random().nextInt(9) }
-        var index = 0
-
-        random.forEach {
-            index++
-            list.addAll(listOf(RandomNumbersModel(it, index)))
-            index--
-        }
-        adapter.submitList(list)
         setUpBtnBack()
         setUpBtnNextAndBtnPreviousListener()
+        setUpBtnFinish()
+
     }
 
+    private fun setUpBtnFinish() {
+        binding.btnFinish.setOnSingleClickListener {
+//            if (viewModel.list.lastOrNull { it.numbers < 0 } == null)
+        }
+    }
+
+    override fun setupObserves() {
+        viewModel.randomNumbersState.observe(viewLifecycleOwner) {
+            adapter?.submitList(it)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
     private fun setUpBtnNextAndBtnPreviousListener() {
         binding.btnNext.setOnSingleClickListener {
-            adapter.setNextAndPreviousItemRow(row + 7)
-            Log.e("anime", "setUpBtnNextAndBtnPreviousListener: $row")
+            if (viewModel.list.lastIndex != args.quantitynumber) {
+                row += 7
+                last += 7
+                adapter?.setNextAndPreviousItemRow(row, last)
+                adapter?.notifyDataSetChanged()
+                Log.e("anime", "Plus: $row")
+            } else {
+                binding.btnFinish.gone()
+            }
         }
         binding.btnPrevious.setOnSingleClickListener {
-            adapter.setNextAndPreviousItemRow(row - 7)
+            if (row >= 7 && last >= 14) {
+                row -= 7
+                last -= 7
+                adapter?.setNextAndPreviousItemRow(row, last)
+                Log.e("anime", "Minus: $row")
+                adapter?.notifyDataSetChanged()
+            } else {
+                row = 7
+                last = 14
+            }
         }
     }
 
@@ -60,7 +82,12 @@ class GameRandomNumbersFragment :
         binding.btnBack.setOnSingleClickListener {
             findNavController().navigate(R.id.action_gameRandomNumbersFragment_to_homeFragment)
         }
+
     }
 
-
+    @SuppressLint("RestrictedApi")
+    override fun onDestroyView() {
+        super.onDestroyView()
+        adapter = null
+    }
 }
