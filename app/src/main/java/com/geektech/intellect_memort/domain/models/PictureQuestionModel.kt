fb1fff2aa831862e96c4329aa85c96a16c6408d0
mura-sv <miura.usbv@gmail.com>
2022-03-22 17:21:33 +0600

    question model

---
+++ app/src/main/java/com/geektech/intellect_memort/domain/models/PictureQuestionModel.kt
@@ -0,0 +1,12 @@
+package com.geektech.intellect_memort.domain.models
+
+data class PictureQuestionModel(
+
+    val imageModel : PictureImageModel,
+    val firstAnswer : String?,
+    val secondAnswer : String?,
+    val thirdAnswer : String?,
+    val fourthAnswer : String?,
+    val correctAnswer : String?,
+
+)
