65b8ae33708476be6a2e8b32dcd27305d450442e
mura-sv <miura.usbv@gmail.com>
2022-03-22 17:19:37 +0600

    view model setup

---
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/picture/game/answering/PicturePlayGameAnsweringViewModel.kt
@@ -0,0 +1,13 @@
+package com.geektech.intellect_memort.presentation.ui.fragments.picture.game.answering
+
+import com.geektech.intellect_memort.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PicturePlayGameAnsweringViewModel @Inject constructor
+    () : BaseViewModel() {
+
+
+
+}
