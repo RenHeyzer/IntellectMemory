5906eb743af5aeb3f425d25eb6beba6687a4c287
mura-sv <miura.usbv@gmail.com>
2022-03-22 17:22:41 +0600

    images list adapter in pao

---
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/picture/game/gameplay/PlayImageListAdapter.kt
@@ -0,0 +1,66 @@
+package com.geektech.intellect_memort.presentation.ui.fragments.picture.game.gameplay
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.geektech.intellect_memort.databinding.PictureRvItemBinding
+import com.geektech.intellect_memort.domain.models.PictureImageModel
+import com.geektech.intellect_memort.presentation.ui.fragments.picture.game.gameplay.PlayImageListAdapter.PlayImageViewHolder
+
+class PlayImageListAdapter : ListAdapter<PictureImageModel, PlayImageViewHolder>(diffCallback) {
+
+    private var onItemClickListener: ((PictureImageModel) -> Unit)? = null
+
+    fun setOnClickListener(listener: (PictureImageModel) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayImageViewHolder {
+        return PlayImageViewHolder(PictureRvItemBinding
+            .inflate(LayoutInflater
+                .from(parent.context),
+                parent,
+                false))
+    }
+
+
+    override fun onBindViewHolder(holder: PlayImageViewHolder, position: Int) {
+        getItem(position)?.let { holder.onBind(it) }
+    }
+
+
+    inner class PlayImageViewHolder(val binding: PictureRvItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(item: PictureImageModel) {
+            Glide.with(binding.image)
+                .load(item.imageUrl)
+                .into(binding.image)
+            binding.title.text = item.id
+
+
+        }
+    }
+
+
+    companion object {
+        val diffCallback = object : DiffUtil.ItemCallback<PictureImageModel>() {
+            override fun areItemsTheSame(
+                oldItem: PictureImageModel,
+                newItem: PictureImageModel,
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: PictureImageModel,
+                newItem: PictureImageModel,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
