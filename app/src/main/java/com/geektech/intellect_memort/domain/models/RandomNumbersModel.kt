4ed929c88da6d964620547603816d579a5e4df67
RenHeyzer <vesolyyradin@gmail.com>
2022-03-30 23:52:28 +0600

    reformat code, and extend from IBaseDiffModel

--- app/src/main/java/com/geektech/intellect_memort/domain/models/RandomNumbersModel.kt
+++ app/src/main/java/com/geektech/intellect_memort/domain/models/RandomNumbersModel.kt
@@ -2,13 +2,11 @@ package com.geektech.intellect_memort.domain.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.geektech.intellect_memort.common.base.IBaseDiffModel
 
 @Entity(tableName = "random_numbers_model")
 data class RandomNumbersModel(
     @PrimaryKey
-    val id: Int? = null,
+    override val id: Int? = null,
     var number: Int? = null,
-)
-
-fun RandomNumbersModel.toAnswerNumbersModel(): AnswerNumbersModel = AnswerNumbersModel(id, number)
-
+) : IBaseDiffModel
