46feb6b6baa6b843e5a0ad82d027f00406a44bd4
RenHeyzer <vesolyyradin@gmail.com>
2022-04-04 12:07:54 +0600

    rename class, and added button back

--- app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/randomnumbers/InputCountOfNumbersFragment.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/randomnumbers/InputCountOfNumbersFragment.kt
@@ -4,43 +4,54 @@ import android.util.Log
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.intellect_memort.R
 import com.geektech.intellect_memort.common.base.BaseFragment
 import com.geektech.intellect_memort.common.extension.hideKeyboard
-import com.geektech.intellect_memort.databinding.FragmentRandomNumbersBinding
+import com.geektech.intellect_memort.common.extension.setOnSingleClickListener
+import com.geektech.intellect_memort.databinding.FragmentInputCountOfNumbersBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RandomNumbersFragment : BaseFragment<FragmentRandomNumbersBinding, RandomNumbersViewModel>(
-    R.layout.fragment_random_numbers
-) {
-    override val binding by viewBinding(FragmentRandomNumbersBinding::bind)
-    override val viewModel: RandomNumbersViewModel by viewModels()
+class InputCountOfNumbersFragment :
+    BaseFragment<FragmentInputCountOfNumbersBinding, InputCountOfNumbersViewModel>(
+        R.layout.fragment_input_count_of_numbers
+    ) {
+    override val binding by viewBinding(FragmentInputCountOfNumbersBinding::bind)
+    override val viewModel: InputCountOfNumbersViewModel by viewModels()
+    private val args by navArgs<InputCountOfNumbersFragmentArgs>()
     private var quantityNumbers: Int = 0
 
     override fun setupListeners() = with(binding) {
         etQuantityNumbers.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 if (etQuantityNumbers.text?.isNotEmpty() == true && etQuantityNumbers.text.toString()
-                        .toInt() < 1001 && etQuantityNumbers.text.toString().toInt() > 0
+                        .toInt() < 1001 && etQuantityNumbers.text.toString()
+                        .toInt() > 0 && etQuantityNumbers.text[0].code != 0
                 ) {
                     quantityNumbers = etQuantityNumbers.text.toString().toInt()
                     etQuantityNumbers.text?.clear()
                     etQuantityNumbers.hideKeyboard()
                     Log.e("anime", " QuantityNumbers $quantityNumbers")
                     findNavController().navigate(
-                        RandomNumbersFragmentDirections.actionRandomNumbersFragmentToTimeToRememberFragment(
-                            quantityNumbers
+                        InputCountOfNumbersFragmentDirections.actionRandomNumbersFragmentToTimeToRememberFragment(
+                            quantityNumbers,
+                            args.isBinary
                         )
                     )
                 } else {
-//                    etQuantityNumbers.error = ,
+                    etQuantityNumbers.error = getString(R.string.error_text_input_admissible_number)
                 }
             }
             false
         }
-
+        setupBackListener()
     }
 
+    private fun setupBackListener() {
+        binding.btnBack.setOnSingleClickListener {
+            findNavController().navigateUp()
+        }
+    }
 }
