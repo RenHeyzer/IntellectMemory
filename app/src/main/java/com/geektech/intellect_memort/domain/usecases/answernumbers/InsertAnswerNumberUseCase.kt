869fafcbe858e916fbc876f5f7dd73e249651493
RenHeyzer <vesolyyradin@gmail.com>
2022-04-04 12:08:25 +0600

    minor changes

--- app/src/main/java/com/geektech/intellect_memort/domain/usecases/answernumbers/InsertAnswerNumberUseCase.kt
+++ app/src/main/java/com/geektech/intellect_memort/domain/usecases/answernumbers/InsertAnswerNumberUseCase.kt
@@ -1,11 +1,11 @@
 package com.geektech.intellect_memort.domain.usecases.answernumbers
 
 import com.geektech.intellect_memort.domain.models.AnswerNumbersModel
-import com.geektech.intellect_memort.domain.repositories.AnswerRandomNumbersRepository
+import com.geektech.intellect_memort.domain.repositories.AnswerRandomRepository
 import javax.inject.Inject
 
 class InsertAnswerNumberUseCase @Inject constructor(
-    private val repository: AnswerRandomNumbersRepository,
+    private val repository: AnswerRandomRepository,
 ) {
 
     suspend fun execute(numbers: List<AnswerNumbersModel>) =
