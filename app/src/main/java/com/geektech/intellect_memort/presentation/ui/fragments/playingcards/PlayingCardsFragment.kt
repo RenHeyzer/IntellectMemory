489e691a47ced25b61aa0b8983e344b8f60c0903
Admin <baconnkl@gmail.com>
2022-04-25 20:17:03 +0600

    added parameters strings

--- app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/playingcards/PlayingCardsFragment.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/playingcards/PlayingCardsFragment.kt
@@ -11,6 +11,7 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.intellect_memort.R
 import com.geektech.intellect_memort.common.base.BaseFragment
 import com.geektech.intellect_memort.common.extension.navigateSafely
+import com.geektech.intellect_memort.common.extension.overrideOnBackPressed
 import com.geektech.intellect_memort.common.extension.setOnSingleClickListener
 import com.geektech.intellect_memort.databinding.FragmentPlayingCardsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,17 +27,24 @@ class PlayingCardsFragment :
     private var isClickedBtn2 = false
     private var isClickedBtn3 = false
     private var isClickedBtn4 = false
+    private var brick = ""
+    private var clover = ""
+    private var piqui = ""
+    private var redHeart = ""
 
     @SuppressLint("ClickableViewAccessibility")
     override fun setupListeners() = with(binding) {
+        setUpBtnBack()
         btnCards1.setOnTouchListener { _, motionEvent ->
             when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
                     isClickedBtn1 = if (!isClickedBtn1) {
                         cardsBtn1.setStrokeColor(ColorStateList.valueOf(Color.parseColor("#F1C256")))
+                        clover = "clover"
                         true
                     } else {
                         cardsBtn1.setStrokeColor(ColorStateList.valueOf(Color.TRANSPARENT))
+                        clover = ""
                         false
                     }
 
@@ -51,9 +59,11 @@ class PlayingCardsFragment :
                 MotionEvent.ACTION_DOWN -> {
                     isClickedBtn2 = if (!isClickedBtn2) {
                         cardsBtn2.setStrokeColor(ColorStateList.valueOf(Color.parseColor("#F1C256")))
+                        redHeart = "red_heart"
                         true
                     } else {
                         cardsBtn2.setStrokeColor(ColorStateList.valueOf(Color.TRANSPARENT))
+                        redHeart = ""
                         false
                     }
                 }
@@ -67,9 +77,11 @@ class PlayingCardsFragment :
                 MotionEvent.ACTION_DOWN -> {
                     isClickedBtn3 = if (!isClickedBtn3) {
                         cardsBtn3.setStrokeColor(ColorStateList.valueOf(Color.parseColor("#F1C256")))
+                        piqui = "piqui"
                         true
                     } else {
                         cardsBtn3.setStrokeColor(ColorStateList.valueOf(Color.TRANSPARENT))
+                        piqui = ""
                         false
                     }
                 }
@@ -84,9 +96,11 @@ class PlayingCardsFragment :
                 MotionEvent.ACTION_DOWN -> {
                     isClickedBtn4 = if (!isClickedBtn4) {
                         cardsBtn4.setStrokeColor(ColorStateList.valueOf(Color.parseColor("#F1C256")))
+                        brick = "brick"
                         true
                     } else {
                         cardsBtn4.setStrokeColor(ColorStateList.valueOf(Color.TRANSPARENT))
+                        brick = ""
                         false
                     }
                 }
@@ -100,16 +114,25 @@ class PlayingCardsFragment :
         }
     }
 
+    private fun setUpBtnBack() {
+        binding.btnBack.setOnSingleClickListener {
+            findNavController().navigateUp()
+        }
+        overrideOnBackPressed {
+            findNavController().navigateUp()
+        }
+    }
+
     private fun setUpBtnChoose() {
         if (isClickedBtn1 || isClickedBtn2 || isClickedBtn3 || isClickedBtn4
         ) {
             findNavController().navigateSafely(
                 PlayingCardsFragmentDirections
                     .actionPlayingCardsFragmentToChooseTheNumberOfCardsFragment(
-                        isClover = isClickedBtn1,
-                        isredHeart = isClickedBtn2,
-                        ispiqui = isClickedBtn3,
-                        isbrick = isClickedBtn4
+                        isClover = clover,
+                        isredHeart = redHeart,
+                        ispiqui = piqui,
+                        isbrick = brick
                     )
             )
         } else if (!isClickedBtn1 || !isClickedBtn2 || !isClickedBtn3 || !isClickedBtn4) {
@@ -123,6 +146,10 @@ class PlayingCardsFragment :
 
     override fun onDestroyView() {
         super.onDestroyView()
+        brick = ""
+        clover = ""
+        redHeart = ""
+        piqui = ""
         isClickedBtn1 = false
         isClickedBtn2 = false
         isClickedBtn3 = false
