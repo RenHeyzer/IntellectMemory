78e6d2655f2e19d75d9335416eb049ccb3c89c05
Admin <baconnkl@gmail.com>
2022-04-25 20:14:05 +0600

    fetch Cards  and minor changes

--- app/src/main/java/com/geektech/intellect_memort/domain/usecases/FetchCardsUseCase.kt
+++ app/src/main/java/com/geektech/intellect_memort/domain/usecases/FetchCardsUseCase.kt
@@ -3,7 +3,9 @@ package com.geektech.intellect_memort.domain.usecases
 import com.geektech.intellect_memort.domain.repositories.CardsRepository
 import javax.inject.Inject
 
-class FetchCardsUseCase @Inject constructor(private val repository: CardsRepository) {
+class FetchCardsUseCase @Inject constructor(
+    private val repository: CardsRepository,
+) {
     operator fun invoke(
         typeClover: String,
         typeBrick: String,
