ba0b5cdfafa9a4cfc08dc7c6bf75d1eb44ccd19d
Admin <baconnkl@gmail.com>
2022-04-25 20:12:51 +0600

    change parameters var

--- app/src/main/java/com/geektech/intellect_memort/presentation/models/CardsUI.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/models/CardsUI.kt
@@ -3,9 +3,9 @@ package com.geektech.intellect_memort.presentation.models
 import com.geektech.intellect_memort.domain.models.CardsModel
 
 data class CardsUI(
-    val url: String? = null,
-    val id: String? = null,
-    val type: String? = null,
+    var url: String? = null,
+    var id: String? = null,
+    var type: String? = null,
 )
 
 fun CardsModel.toUI() = CardsUI(url, id, type)
