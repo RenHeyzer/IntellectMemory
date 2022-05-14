352dddb8f51481a489266463fbfc42ee84260d6f
RenHeyzer <vesolyyradin@gmail.com>
2022-05-14 18:41:41 +0600

    implement Class from Serializable

--- app/src/main/java/com/geektech/intellect_memort/presentation/models/CardsUI.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/models/CardsUI.kt
@@ -1,12 +1,13 @@
 package com.geektech.intellect_memort.presentation.models
 
 import com.geektech.intellect_memort.domain.models.CardsModel
+import java.io.Serializable
 
 data class CardsUI(
     var url: String? = null,
     var id: Int? = null,
     var type: String? = null,
     val emptySpace: Boolean? = null,
-)
+): Serializable
 
 fun CardsModel.toUI() = CardsUI(url, id, type)
