6746f2a9e63f4669cdd4082f26a86307738e8bbb
RenHeyzer <vesolyyradin@gmail.com>
2022-05-27 23:47:22 +0600

    minor changes

--- app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/game/randomnumbers/GameNumbersFragment.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/game/randomnumbers/GameNumbersFragment.kt
@@ -11,11 +11,11 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.intellect_memort.R
 import com.geektech.intellect_memort.common.base.BaseFragment
 import com.geektech.intellect_memort.common.extension.correctNumber
+import com.geektech.intellect_memort.common.extension.navigateSafely
 import com.geektech.intellect_memort.common.extension.setOnSingleClickListener
 import com.geektech.intellect_memort.common.extension.timer
 import com.geektech.intellect_memort.databinding.FragmentGameNumbersBinding
-import com.geektech.intellect_memort.domain.models.NumbersModel
-import com.geektech.intellect_memort.presentation.adapters.NumbersAdapter
+import com.geektech.intellect_memort.presentation.ui.adapters.NumbersAdapter
 import com.geektech.intellect_memort.presentation.state.UIState
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -40,11 +40,10 @@ class GameNumbersFragment :
     }
 
     override fun setupViews() {
-        setupButtonPrevVisibility()
-    }
-
-    private fun setupButtonPrevVisibility() {
-        binding.btnPrevious.isVisible = last != 14
+        binding.btnPrevious.isVisible = false
+        binding.btnNext.isVisible = false
+        binding.btnEndPrevious.isVisible = false
+        binding.btnEndNext.isVisible = true
     }
 
     override fun setupListeners() {
@@ -57,7 +56,16 @@ class GameNumbersFragment :
         val quantityNumber = args.quantitynumber
         binding.btnFinish.setOnSingleClickListener {
             countDownTimer?.cancel()
-            findNavController().navigate(
+            findNavController().navigateSafely(
+                GameNumbersFragmentDirections.actionGameNumbersFragmentToAnswerNumbersFragment(
+                    quantityNumber,
+                    ascendingTimerAsString
+                )
+            )
+        }
+        binding.btnEndPrevious.setOnSingleClickListener {
+            countDownTimer?.cancel()
+            findNavController().navigateSafely(
                 GameNumbersFragmentDirections.actionGameNumbersFragmentToAnswerNumbersFragment(
                     quantityNumber,
                     ascendingTimerAsString
@@ -81,7 +89,7 @@ class GameNumbersFragment :
             ascendingTimerAsString = it
         }, {
             if (ascendingTimerAsString.isNotEmpty()) {
-                findNavController().navigate(
+                findNavController().navigateSafely(
                     GameNumbersFragmentDirections.actionGameNumbersFragmentToAnswerNumbersFragment(
                         args.quantitynumber,
                         ascendingTimerAsString
@@ -111,42 +119,86 @@ class GameNumbersFragment :
                 }
             }
         }
-
         viewModel.saveRandomNumbersState.subscribeIdle {
             adapter?.submitList(it)
         }
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    private fun setUpBtnNextAndBtnPreviousListener() {
+    private fun setUpBtnNextAndBtnPreviousListener() = with(binding) {
         adapter?.setNextAndPreviousItemRow(row, last)
-        binding.btnNext.setOnSingleClickListener {
+        btnNext.setOnSingleClickListener {
             row += 7
             last += 7
+            setupButtonsPrevAndNextVisibility()
             adapter?.setNextAndPreviousItemRow(row, last)
             adapter?.notifyDataSetChanged()
             Log.e("anime", "Plus: $last")
         }
-        binding.btnPrevious.setOnSingleClickListener {
+        btnEndNext.setOnSingleClickListener {
+            row += 7
+            last += 7
+            setupButtonsPrevAndNextVisibility()
+            adapter?.setNextAndPreviousItemRow(row, last)
+            adapter?.notifyDataSetChanged()
+            Log.e("anime", "Plus: $last")
+        }
+        btnPrevious.setOnSingleClickListener {
             row -= 7
             last -= 7
+            setupButtonsPrevAndNextVisibility()
             adapter?.setNextAndPreviousItemRow(row, last)
             Log.e("anime", "Minus: $last")
             adapter?.notifyDataSetChanged()
         }
     }
 
+    private fun setupButtonsPrevAndNextVisibility() = with(binding) {
+        adapter?.itemCount?.let {
+            when {
+                last != 6 && last < it -> {
+                    Log.e("lasteed", "1")
+                    btnPrevious.isVisible = true
+                    btnNext.isVisible = true
+                    btnEndNext.isVisible = false
+                    btnEndPrevious.isVisible = false
+                }
+                last != 6 && last > it -> {
+                    btnPrevious.isVisible = false
+                    btnNext.isVisible = false
+                    Log.e("lasteed", "2")
+                    btnEndNext.isVisible = false
+                    btnEndPrevious.isVisible = true
+                }
+                last == 6 && last < it -> {
+                    Log.e("lasteed", "else")
+                    btnPrevious.isVisible = false
+                    btnNext.isVisible = false
+                    btnEndNext.isVisible = true
+                    btnEndPrevious.isVisible = false
+                }
+            }
+        }
+        Log.e("nummers", adapter?.itemCount.toString())
+    }
+
     private fun setUpBtnBack() {
         binding.btnBack.setOnSingleClickListener {
             Log.e("sizing", "fragment: ${adapter?.itemCount}")
-            findNavController().navigate(
+            findNavController().navigateSafely(
                 GameNumbersFragmentDirections.actionGameNumbersFragmentToExitDialogFragment(false)
             )
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        countDownTimer?.cancel()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         adapter = null
+        countDownTimer?.cancel()
     }
 }
