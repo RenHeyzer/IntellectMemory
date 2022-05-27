cbe171c6f9fb275d2670e51177e9918acae867ec
RenHeyzer <vesolyyradin@gmail.com>
2022-05-27 23:37:42 +0600

    moved to another package

--- app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/game/randomnumbers/answer/AnswerNumbersFragment.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/game/randomnumbers/answer/AnswerNumbersFragment.kt
@@ -2,16 +2,19 @@ package com.geektech.intellect_memort.presentation.ui.fragments.game.randomnumbe
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.intellect_memort.R
 import com.geektech.intellect_memort.common.base.BaseFragment
+import com.geektech.intellect_memort.common.extension.correctNumber
+import com.geektech.intellect_memort.common.extension.navigateSafely
 import com.geektech.intellect_memort.common.extension.setOnSingleClickListener
 import com.geektech.intellect_memort.databinding.FragmentAnswerNumbersBinding
 import com.geektech.intellect_memort.domain.models.AnswerNumbersModel
-import com.geektech.intellect_memort.presentation.adapters.AnswerNumbersAdapter
+import com.geektech.intellect_memort.presentation.ui.adapters.AnswerNumbersAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,6 +39,13 @@ class AnswerNumbersFragment :
         }
     }
 
+    override fun setupViews() {
+        binding.btnPrevious.isVisible = false
+        binding.btnNext.isVisible = false
+        binding.btnEndFinish.isVisible = false
+        binding.btnEndNext.isVisible = true
+    }
+
     override fun setupListeners() {
         setupBackClickListener()
         setUpBtnNextAndBtnPreviousListener()
@@ -44,7 +54,7 @@ class AnswerNumbersFragment :
 
     private fun setupBackClickListener() {
         binding.btnBack.setOnSingleClickListener {
-            findNavController().navigate(
+            findNavController().navigateSafely(
                 AnswerNumbersFragmentDirections.actionAnswerNumbersFragmentToExitDialogFragment(true)
             )
         }
@@ -53,7 +63,16 @@ class AnswerNumbersFragment :
     private fun setupFinishClickListener() {
         binding.btnResult.setOnSingleClickListener {
             viewModel.insertAllAnswerOfNumbers(answerList)
-            findNavController().navigate(
+            findNavController().navigateSafely(
+                AnswerNumbersFragmentDirections.actionAnswerNumbersFragmentToResultNumbersFragment(
+                    args.timeToRemember,
+                    binding.txtTimer.text.toString()
+                )
+            )
+        }
+        binding.btnEndFinish.setOnSingleClickListener {
+            viewModel.insertAllAnswerOfNumbers(answerList)
+            findNavController().navigateSafely(
                 AnswerNumbersFragmentDirections.actionAnswerNumbersFragmentToResultNumbersFragment(
                     args.timeToRemember,
                     binding.txtTimer.text.toString()
@@ -67,10 +86,11 @@ class AnswerNumbersFragment :
     }
 
     override fun setupObserves() {
+        val correctQuantity = args.size.correctNumber()
         viewModel.randomNumbersState.subscribeIdle {
             it.forEach { model ->
                 model.id?.let { id ->
-                    if (answerList.size <= args.size) {
+                    if (answerList.size <= correctQuantity) {
                         answerList.add(AnswerNumbersModel(id, null))
                         identifierList.add(id)
                     }
@@ -83,23 +103,60 @@ class AnswerNumbersFragment :
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    private fun setUpBtnNextAndBtnPreviousListener() {
+    private fun setUpBtnNextAndBtnPreviousListener() = with(binding) {
         adapter?.setNextAndPrevious(lastPosition, rowLastPosition)
-        binding.btnNext.setOnSingleClickListener {
+        btnNext.setOnSingleClickListener {
             lastPosition += 7
             rowLastPosition += 7
+            setupButtonsPrevAndNextVisibility()
             adapter?.setNextAndPrevious(lastPosition, rowLastPosition)
             adapter?.notifyDataSetChanged()
         }
-        binding.btnPrevious.setOnSingleClickListener {
+        btnPrevious.setOnSingleClickListener {
             if (lastPosition >= 6 || rowLastPosition >= 0) {
                 lastPosition -= 7
                 rowLastPosition -= 7
+                setupButtonsPrevAndNextVisibility()
                 adapter?.setNextAndPrevious(lastPosition, rowLastPosition)
                 adapter?.notifyDataSetChanged()
             } else {
                 lastPosition = 6
                 rowLastPosition = 0
+            }
+        }
+        btnEndNext.setOnSingleClickListener {
+            lastPosition += 7
+            rowLastPosition += 7
+            setupButtonsPrevAndNextVisibility()
+            adapter?.setNextAndPrevious(lastPosition, rowLastPosition)
+            adapter?.notifyDataSetChanged()
+        }
+    }
+
+    private fun setupButtonsPrevAndNextVisibility() = with(binding) {
+        adapter?.itemCount?.let {
+            when {
+                lastPosition != 6 && lastPosition < it -> {
+                    Log.e("lasteed", "1")
+                    btnPrevious.isVisible = true
+                    btnNext.isVisible = true
+                    btnEndNext.isVisible = false
+                    btnEndFinish.isVisible = false
+                }
+                lastPosition != 6 && lastPosition > it -> {
+                    btnPrevious.isVisible = false
+                    btnNext.isVisible = false
+                    Log.e("lasteed", "2")
+                    btnEndNext.isVisible = false
+                    btnEndFinish.isVisible = true
+                }
+                lastPosition == 6 && lastPosition < it -> {
+                    Log.e("lasteed", "else")
+                    btnPrevious.isVisible = false
+                    btnNext.isVisible = false
+                    btnEndNext.isVisible = true
+                    btnEndFinish.isVisible = false
+                }
             }
         }
     }
