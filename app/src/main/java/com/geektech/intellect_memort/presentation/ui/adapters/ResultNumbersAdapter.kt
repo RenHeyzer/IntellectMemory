180d5cc09e306d376fb3e987cc95f65308e82827
RenHeyzer <vesolyyradin@gmail.com>
2022-05-28 00:06:47 +0600

    moved to another package

--- app/src/main/java/com/geektech/intellect_memort/presentation/ui/adapters/ResultNumbersAdapter.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/adapters/ResultNumbersAdapter.kt
@@ -1,4 +1,4 @@
-package com.geektech.intellect_memort.presentation.adapters
+package com.geektech.intellect_memort.presentation.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
