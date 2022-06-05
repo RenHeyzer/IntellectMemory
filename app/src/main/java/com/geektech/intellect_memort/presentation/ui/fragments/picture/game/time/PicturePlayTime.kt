2996631a01a9d10f8c2aeddaaba205e1f53c1b54
mura-sv <miura.usbv@gmail.com>
2022-06-05 19:30:16 +0600

    picture/pao next button fixed picture/countdown set from 5 to 3 seconds picture/time settings changed from 10 to 60minutes

--- app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/picture/game/time/PicturePlayTime.kt
+++ app/src/main/java/com/geektech/intellect_memort/presentation/ui/fragments/picture/game/time/PicturePlayTime.kt
@@ -16,7 +16,7 @@ class PicturePlayTime : Fragment(R.layout.fragment_picture_play_time) {
     private val binding by viewBinding(FragmentPicturePlayTimeBinding::bind)
     private val args: PicturePlayTimeArgs by navArgs()
     private var timeNumber: Int? = null
-
+    private var maxMinutesPerGame : Int = 60
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -29,19 +29,19 @@ class PicturePlayTime : Fragment(R.layout.fragment_picture_play_time) {
             if (time.toString().isNotEmpty()) {
                 timeNumber = time.toString().trim().toInt()
             }
-
         }
     }
 
     private fun setupListeners() {
         binding.btnNext.setOnClickListener {
-            if (timeNumber != null) {
+            if (timeNumber != null && timeNumber != 0
+                && timeNumber!! <= maxMinutesPerGame) {
                 findNavController()
                     .navigate(PicturePlayTimeDirections
                         .actionPicturePlayTimeToPicturePlayCountDown(args.rangeNumber,
                             timeNumber!!))
             } else {
-                Toast.makeText(requireContext(), "Введите время", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Введите время от 1 до 60 минут", Toast.LENGTH_SHORT).show()
             }
         }
     }
